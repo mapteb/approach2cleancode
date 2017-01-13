bfd0680381dce85df7a4cb5565aa75dc854c1611
Nalla Senthilnathan <nrsenthil@gmail.com>
2017-01-13 18:15:19 -0500

    Create customizePizza.js

---
+++ src/main/resources/static/js/customizePizza.js
@@ -0,0 +1,57 @@
+var customizePizza = angular.module('pizzaApp', []);
+
+
+customizePizza.controller('pizza', function($scope, $http) {
+	var self = this;
+    $scope.list = [];
+    
+    
+    $scope.submitForm = function() {
+
+      if ($scope.pizza) {
+    	  
+    	  
+      	
+        $scope.list.push(this.text);
+        
+        if($scope.pizza.toppingMushroom)$scope.pizza.toppingMushroom='Y';
+        if($scope.pizza.toppingPepperoni)$scope.pizza.toppingPepperoni='Y';
+        if($scope.pizza.toppingSpinach)$scope.pizza.toppingSpinach='Y';
+        $scope.pizza.nextView=new Date();
+        
+        var urlstr = "//springboot-rest-jpa-nsorg.cfapps.io";
+        //urlstr = "http://localhost:8081"
+        //cache buster param
+        var dt=(new Date()).getSeconds();
+        
+        $http({
+
+            url: urlstr + "/pizza?dt="+dt,
+            
+
+            data: $scope.pizza,
+            method: 'POST',
+            headers : {'Content-Type':'application/json; charset=UTF-8'}
+
+        }).success(function(data){
+
+            console.log("OK", data);
+            
+
+            window.location.href='reviewCart.html';
+            
+            
+
+        }).error(function(err){
+        	"ERR", console.log(err); 
+        			
+            //display success message
+            $scope.pizza.status = "Submit Error";
+            $scope.pizza.status_color = "label-danger"; 
+
+        	});
+        
+      }};
+    });
+
+
