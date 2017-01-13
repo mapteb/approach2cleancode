b9aecc125fd901ab4c781ea120005944dba8abcc
Nalla Senthilnathan <nrsenthil@gmail.com>
2017-01-13 18:16:43 -0500

    Create cart.js

---
+++ src/main/resources/static/js/cart.js
@@ -0,0 +1,22 @@
+var customizePizza = angular.module('cartApp', []);
+
+customizePizza.controller('cart', function($scope, $http) {
+	
+	var self = this;
+	
+	//cache buster param
+	var dt = (new Date()).getSeconds();
+	
+
+    var urlstr = "//springboot-rest-jpa-nsorg.cfapps.io";
+    //urlstr = "http://localhost:8081"
+	
+	url = urlstr + '/cart/items/count?dt='+dt;
+	
+	$http.get(url).then(function(response) {
+		self.items = response.data;
+	}, function(response){alert('?? err');});
+	
+	
+});
+
